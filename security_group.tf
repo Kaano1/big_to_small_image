790e561b8396aa7e4e5b91a2305bef7ded163df1
Kaano1 <ahmetyumusakdiken13@gmail.com>
2024-10-07 16:41:54 +0300

    Fixed: security groups set 3306 port and some security group

--- security_group.tf
+++ security_group.tf
@@ -1,6 +1,16 @@
 
 # Security Group
 
+resource "aws_security_group" "rds_security_group" {
+	vpc_id = aws_vpc.terraform_vpc.id
+	name = "rds_security_group"
+	description = "Allow inbound and outbound traffic"
+
+	tags = {
+		Name = "rds_security_group"
+	}
+}
+
 resource "aws_security_group" "security_group" {
 	vpc_id = aws_vpc.terraform_vpc.id
 	name = "terraform_security_group"
@@ -11,7 +21,30 @@ resource "aws_security_group" "security_group" {
 	}
 }
 
-# Security Group Egress Rules
+# rds_security_group Egress Rules
+
+resource "aws_security_group_rule" "rds_set_egress-1" {
+	type 			= "egress"
+	from_port		= 3306
+	to_port			= 3306
+	protocol		= "tcp"
+	security_group_id = aws_security_group.rds_security_group.id
+	source_security_group_id = aws_security_group.security_group.id
+}
+
+# rds_security_group Ingress Rules
+
+
+resource "aws_security_group_rule" "rds_set_ingress-1" {
+	type 			= "ingress"
+	from_port		= 3306
+	to_port			= 3306
+	protocol		= "tcp"
+	security_group_id = aws_security_group.rds_security_group.id
+	source_security_group_id = aws_security_group.security_group.id
+}
+
+# security_group Egress Rules
 
 resource "aws_security_group_rule" "set_egress-1" {
   type              = "egress"
@@ -22,11 +55,10 @@ resource "aws_security_group_rule" "set_egress-1" {
   cidr_blocks      = ["0.0.0.0/0"]
 }
 
-
-# Security Group Ingress Rules
+# security_group Ingress Rules
 resource "aws_vpc_security_group_ingress_rule" "set_ingress-1" {
 	security_group_id = aws_security_group.security_group.id
-	cidr_ipv4 = aws_vpc.terraform_vpc.cidr_block
+	cidr_ipv4 = "0.0.0.0/0"
 	from_port = 443
 	to_port = 443
 	ip_protocol = "tcp"
@@ -34,7 +66,7 @@ resource "aws_vpc_security_group_ingress_rule" "set_ingress-1" {
 
 resource "aws_vpc_security_group_ingress_rule" "set_ingress-2" {
 	security_group_id = aws_security_group.security_group.id
-	cidr_ipv4 = aws_vpc.terraform_vpc.cidr_block
+	cidr_ipv4 = "0.0.0.0/0"
 	from_port = 80
 	to_port = 80
 	ip_protocol = "tcp"
@@ -42,7 +74,7 @@ resource "aws_vpc_security_group_ingress_rule" "set_ingress-2" {
 
 resource "aws_vpc_security_group_ingress_rule" "set_ingress-3" {
 	security_group_id = aws_security_group.security_group.id
-	cidr_ipv4 = aws_vpc.terraform_vpc.cidr_block
+	cidr_ipv4 = "0.0.0.0/0"
 	from_port = 22
 	to_port = 22
 	ip_protocol = "tcp"
@@ -50,8 +82,26 @@ resource "aws_vpc_security_group_ingress_rule" "set_ingress-3" {
 
 resource "aws_vpc_security_group_ingress_rule" "set_ingress-4" {
 	security_group_id = aws_security_group.security_group.id
-	cidr_ipv4 = aws_vpc.terraform_vpc.cidr_block
+	cidr_ipv4 = "0.0.0.0/0"
 	ip_protocol = "icmp"
 	from_port = -1
 	to_port = -1
 }
+
+resource "aws_vpc_security_group_ingress_rule" "set_ingress-5" {
+	security_group_id = aws_security_group.security_group.id
+	cidr_ipv4 = "0.0.0.0/0"
+	ip_protocol = "tcp"
+	from_port = 2049
+	to_port = 2049
+}
+
+resource "aws_security_group_rule" "allow_security_group" {
+  type                     = "ingress"
+  from_port                = 3306       
+  to_port                  = 3306        
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.rds_security_group.id
+  security_group_id        = aws_security_group.security_group.id
+}
+
