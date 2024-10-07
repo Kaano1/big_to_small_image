eaf71de8271c5df40e6e65589fc4583baa21cb60
Kaano1 <ahmetyumusakdiken13@gmail.com>
2024-10-07 16:30:24 +0300

    Added: right now we can create ec2 instance

---
+++ ec2-instances.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "terraform-ec2-instance" {
+	ami = "ami-097c5c21a18dc59ea" # Amazon Linux 2 AMI
+	instance_type = "t3.micro" # Free tier eligible
+	vpc_security_group_ids  = [aws_security_group.security_group.id]
+	subnet_id = aws_subnet.terraform_public_ip_1.id
+	iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+	key_name = "myComputer"
+
+	tags = {
+		Name = "terraform-ec2-instance"
+	}
+
+	depends_on = [aws_security_group.security_group, aws_subnet.terraform_public_ip_1, aws_iam_instance_profile.instance_profile]
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+	name = "instance-profile"
+	role = aws_iam_role.Ec2-S33.name
+
+	depends_on = [aws_iam_role.Ec2-S33]
+}
